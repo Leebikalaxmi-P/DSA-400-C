a8412d1b6f5e5cc851a535d105f018bca03282b2
Leebikalaxmi-P <leebikalaxmips2607@gmail.com>
2026-03-10 22:42:21 +0530

    Added first  Basic DSA program

---
+++ ODD(OR)E.C
@@ -0,0 +1,21 @@
+/*
+PROBLEM LEVEL: EASY
+PROBLEM: Find odd or Even
+PLATFORM: Geeksforgeeks
+DATE: 10.03.2026
+LANGUAGE:C
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("FIND ODD OR EVEN\n");
+printf("Enter Number:");
+scanf("%d",&n);
+if(n%2==0){
+printf("Even Number");}
+else
+printf("Odd Number");
+return 0;
+}
